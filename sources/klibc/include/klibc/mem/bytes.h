16a898033ebb4ed08278e05ec788032f7ea58a31
David Delassus <david.jose.delassus@gmail.com>
2026-01-17 06:50:37 +0000

    :parkles: add memory intrinsics to klibc

---
+++ sources/klibc/include/klibc/mem/bytes.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stddef.h>
+
+
+void *memcpy (void *restrict dst, const void *restrict src, size_t n);
+void *memmove(void *dst, const void *src, size_t n);
+void *memset (void *dst, int c, size_t n);
+int   memcmp (const void *a, const void *b, size_t n);
