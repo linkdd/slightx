2fd28f4f4a00c6063d2f28a20d1c86bfa4f8d27f
David Delassus <david.jose.delassus@gmail.com>
2026-01-18 18:57:56 +0000

    :sparkles: add console builtin driver

---
+++ sources/kernel/include/kernel/drivers/console.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <klibc/mem/str.h>
+
+
+void console_init (void);
+void console_write(str s);
---
+++ sources/kernel/src/drivers/console.c
@@ -0,0 +1,73 @@
+#include <limine.h>
+#include <flanterm.h>
+#include <flanterm_backends/fb.h>
+
+#include <klibc/mem/bytes.h>
+
+#include <kernel/drivers/console.h>
+#include <kernel/halt.h>
+
+
+static struct flanterm_context *ft_ctx = NULL;
+
+
+void console_init(void) {
+  LIMINE_GET_RESP(framebuffer);
+
+  bool fb_available = (
+    framebuffer_response != NULL &&
+    framebuffer_response->framebuffer_count > 0
+  );
+
+  if (!fb_available) {
+    halt();
+  }
+
+
+  struct limine_framebuffer *fb = framebuffer_response->framebuffers[0];
+
+  ft_ctx = flanterm_fb_init(
+    /* malloc */ NULL,
+    /* free */   NULL,
+    fb->address,
+
+    fb->width,
+    fb->height,
+    fb->pitch,
+
+    fb->red_mask_size,   fb->red_mask_shift,
+    fb->green_mask_size, fb->green_mask_shift,
+    fb->blue_mask_size,  fb->blue_mask_shift,
+
+    /* canvas */ NULL,
+
+    /* ansi_colours */        NULL,
+    /* ansi_bright_colours */ NULL,
+
+    /* default_bg */        NULL,
+    /* default_fg */        NULL,
+    /* default_bg_bright */ NULL,
+    /* default_fg_bright */ NULL,
+
+    /* font */         NULL,
+    /* font_width */   0,
+    /* font_height */  0,
+    /* font_spacing */ 1,
+    /* font_scale_x */ 0,
+    /* font_scale_y */ 0,
+
+    /* margin */       0,
+    FLANTERM_FB_ROTATE_0
+  );
+
+  if (ft_ctx == NULL) {
+    halt();
+  }
+}
+
+
+void console_write(str s) {
+  if (ft_ctx != NULL) {
+    flanterm_write(ft_ctx, s.data, s.length);
+  }
+}
