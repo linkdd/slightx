dade734c8b2d677f6bdeff6d5f467b146ac7130e
David Delassus <david.jose.delassus@gmail.com>
2026-01-19 00:37:03 +0000

    :sparkles: print bootloader and kernel executable info

--- sources/kernel/src/main.c
+++ sources/kernel/src/main.c
@@ -1,6 +1,32 @@
+#include <limine.h>
+
+#include <klibc/assert.h>
+#include <klibc/types.h>
+#include <klibc/io/log.h>
+
 #include <kernel/halt.h>
+
+#include <kernel/drivers/console.h>
 
 
 void kmain(void) {
+  console_init();
+
+  LIMINE_GET_RESP(bootloader_info);
+  assert_release(bootloader_info_response != NULL);
+  klog(
+    "bootloader: %s %s\n",
+    strview_from_cstr(bootloader_info_response->name),
+    strview_from_cstr(bootloader_info_response->version)
+  );
+
+  LIMINE_GET_RESP(executable_file);
+  assert_release(executable_file_response != NULL);
+  klog(
+    "[slightx]# %s %s\n",
+    strview_from_cstr(executable_file_response->executable_file->path),
+    strview_from_cstr(executable_file_response->executable_file->string)
+  );
+
   halt();
 }
