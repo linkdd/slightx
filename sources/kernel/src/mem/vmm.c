787562dd6d5b6225a9f80e55fea6530902ef5d4d
David Delassus <david.jose.delassus@gmail.com>
2026-01-19 16:54:46 +0000

    :recycle: use physical memory manager to allocate page tables

--- sources/kernel/src/mem/vmm.c
+++ sources/kernel/src/mem/vmm.c
@@ -5,6 +5,7 @@
 #include <klibc/assert.h>
 
 #include <kernel/mem/vmm.h>
+#include <kernel/mem/pmm.h>
 #include <kernel/mem/hhdm.h>
 #include <kernel/panic.h>
 
@@ -58,17 +59,17 @@ static inline usize pml1_index(virtual_address va) {
 
 // MARK: pte helpers
 static inline bool pte_present(page_table_entry pte) {
-  return (pte & PT_FLAG_VALID) != 0;
+  return (pte & MM_PT_FLAG_VALID) != 0;
 }
 
 
 static inline bool pte_large(page_table_entry pte) {
-  return (pte & PT_FLAG_LARGE) != 0;
+  return (pte & MM_PT_FLAG_LARGE) != 0;
 }
 
 
 static inline page_table_entry *pte_p2v(page_table_entry pte) {
-  physical_address pa = { .addr = pte & PT_PADDR_MASK };
+  physical_address pa = { .addr = pte & MM_PT_PADDR_MASK };
   virtual_address  va = hhdm_p2v(pa);
   return (page_table_entry*)va.ptr;
 }
@@ -77,12 +78,11 @@ static inline page_table_entry *pte_p2v(page_table_entry pte) {
 // MARK: pd helpers
 static inline page_table_entry *pd_split(
   page_table_entry *pd,
-  allocator         a,
 
-  usize    index,
-  usize    old_page_size,
-  usize    new_page_size,
-  pt_flags table_flags
+  usize     index,
+  page_size old_page_size,
+  page_size new_page_size,
+  pt_flags  table_flags
 ) {
   page_table_entry pte = pd[index];
 
@@ -91,29 +91,28 @@ static inline page_table_entry *pd_split(
   }
 
   if (!(
-    (old_page_size == PAGE_1GB_SIZE && new_page_size == PAGE_2MB_SIZE) ||
-    (old_page_size == PAGE_2MB_SIZE && new_page_size == PAGE_4KB_SIZE)
+    (old_page_size == MM_PAGE_1GB && new_page_size == MM_PAGE_2MB) ||
+    (old_page_size == MM_PAGE_2MB && new_page_size == MM_PAGE_4KB)
   )) {
     panic("[vmm] pd_split(): called with invalid page sizes");
   }
 
-  const uptr phys_base  = pte & PT_PADDR_MASK;
-  u64        leaf_flags = PT_TO_VMM_FLAGS(pte) | PT_FLAG_VALID;
+  const uptr phys_base  = pte & MM_PT_PADDR_MASK;
+  u64        leaf_flags = MM_PT_DEFAULT_FLAGS(pte) | MM_PT_FLAG_VALID;
 
-  if (new_page_size == PAGE_2MB_SIZE) {
-    leaf_flags |= PT_FLAG_LARGE;
+  if (new_page_size == MM_PAGE_2MB) {
+    leaf_flags |= MM_PT_FLAG_LARGE;
   }
 
-  page_table_entry *child = allocate_aligned(a, PT_SIZE, PT_SIZE);
+  physical_address  child_paddr = pmm_alloc(1);
+  virtual_address   child_vaddr = hhdm_p2v(child_paddr);
+  page_table_entry *child       = child_vaddr.ptr;
 
   const usize count = old_page_size / new_page_size;
 
   for (usize i = 0; i < count; i++) {
     child[i] = (page_table_entry)((phys_base + (i * new_page_size)) | leaf_flags);
   }
-
-  virtual_address  child_vaddr = { .ptr = child };
-  physical_address child_paddr = hhdm_v2p(child_vaddr);
 
   pd[index] = (page_table_entry)(child_paddr.addr | table_flags);
 
@@ -129,7 +128,6 @@ static inline page_table_entry *pd_get(page_table_entry *pd, usize index) {
 
 static inline page_table_entry *pd_get_or_create(
   page_table_entry *pd,
-  allocator         a,
 
   usize          index,
   page_map_level level,
@@ -138,26 +136,24 @@ static inline page_table_entry *pd_get_or_create(
   page_table_entry pte = pd[index];
 
   if (!pte_present(pte)) {
-    page_table_entry *t = allocate_aligned(a, PT_SIZE, PT_SIZE);
-
-    virtual_address  t_vaddr = { .ptr = t };
-    physical_address t_paddr = hhdm_v2p(t_vaddr);
+    physical_address t_paddr = pmm_alloc(1);
+    virtual_address  t_vaddr = hhdm_p2v(t_paddr);
 
     pd[index] = (page_table_entry)(t_paddr.addr | table_flags);
-    return t;
+    return t_vaddr.ptr;
   }
 
   if (!pte_large(pte)) {
-    if ((table_flags & PT_FLAG_USER) && !(pte & PT_FLAG_USER)) {
-      pd[index] |= PT_FLAG_USER;
+    if ((table_flags & MM_PT_FLAG_USER) && !(pte & MM_PT_FLAG_USER)) {
+      pd[index] |= MM_PT_FLAG_USER;
     }
 
     return pte_p2v(pte);
   }
 
   switch (level) {
-    case VMM_PML3: return pd_split(pd, a, index, PAGE_1GB_SIZE, PAGE_2MB_SIZE, table_flags);
-    case VMM_PML2: return pd_split(pd, a, index, PAGE_2MB_SIZE, PAGE_4KB_SIZE, table_flags);
+    case PML3: return pd_split(pd, index, MM_PAGE_1GB, MM_PAGE_2MB, table_flags);
+    case PML2: return pd_split(pd, index, MM_PAGE_2MB, MM_PAGE_4KB, table_flags);
     default:
       panic("[vmm] pd_get_or_create(): invalid level for split");
   }
@@ -209,47 +205,50 @@ void vmm_load(void) {
 
   kernel_page_map.directory = (page_table_entry*)pmd_vaddr.ptr;
   kernel_page_map.level     = (paging_mode_response->mode == LIMINE_PAGING_MODE_X86_64_5LVL
-    ? VMM_PML5
-    : VMM_PML4
+    ? PML5
+    : PML4
   );
 }
 
+// MARK: accessor
+page_map *vmm_get_kernel_page_map(void) {
+  return &kernel_page_map;
+}
 
 // MARK: map
 static void vmm_map_impl(
-  page_map  *pmap,
-  allocator  a,
+  page_map *pmap,
 
   virtual_address  va,
   physical_address pa,
 
-  pt_flags   flags,
-  page_size  size
+  pt_flags  flags,
+  page_size size
 ) {
-  const usize alignment = (
-    size == VMM_PAGE_SIZE_1GB ? PAGE_1GB_ALIGNMENT :
-    size == VMM_PAGE_SIZE_2MB ? PAGE_2MB_ALIGNMENT :
-    /* else */                  PAGE_4KB_ALIGNMENT
+  assert(
+    size == MM_PAGE_4KB ||
+    size == MM_PAGE_2MB ||
+    size == MM_PAGE_1GB
   );
 
   assert_release(
-    is_ptr_aligned(va.addr, alignment) &&
-    is_ptr_aligned(pa.addr, alignment)
+    is_ptr_aligned(va.addr, size) &&
+    is_ptr_aligned(pa.addr, size)
   );
 
-  flags                      |= PT_FLAG_VALID;
-  const pt_flags table_flags  = (flags & PT_FLAG_USER) ? PT_USER_TABLE_FLAGS : PT_KERN_TABLE_FLAGS;
+  flags                      |= MM_PT_FLAG_VALID;
+  const pt_flags table_flags  = (flags & MM_PT_FLAG_USER) ? MM_PT_USER_TABLE_FLAGS : MM_PT_KERN_TABLE_FLAGS;
 
   page_table_entry *pml4 = NULL;
   switch (pmap->level) {
-    case VMM_PML5: {
+    case PML5: {
       page_table_entry* pml5 = pmap->directory;
 
-      pml4 = pd_get_or_create(pml5, a, pml5_index(va), VMM_PML5, table_flags);
+      pml4 = pd_get_or_create(pml5, pml5_index(va), PML5, table_flags);
       break;
     }
 
-    case VMM_PML4: {
+    case PML4: {
       pml4 = pmap->directory;
       break;
     }
@@ -258,39 +257,37 @@ static void vmm_map_impl(
       panic("[vmm] vmm_map(): invalid page map level");
   }
 
-  page_table_entry *pml3 = pd_get_or_create(pml4, a, pml4_index(va), VMM_PML4, table_flags);
-  if (size == VMM_PAGE_SIZE_1GB) {
+  page_table_entry *pml3 = pd_get_or_create(pml4, pml4_index(va), PML4, table_flags);
+  if (size == MM_PAGE_1GB) {
     if (vmm__pages_1gb_support()) {
-      pml3[pml3_index(va)] = (page_table_entry)(pa.addr | flags | PT_FLAG_LARGE);
+      pml3[pml3_index(va)] = (page_table_entry)(pa.addr | flags | MM_PT_FLAG_LARGE);
     }
     else {
-      for (isize offset = 0; offset < PAGE_1GB_SIZE; offset += PAGE_2MB_SIZE) {
+      for (isize offset = 0; offset < MM_PAGE_1GB; offset += MM_PAGE_2MB) {
         vmm_map_impl(
           pmap,
-          a,
           (virtual_address) {.addr = va.addr + offset},
           (physical_address){.addr = pa.addr + offset},
           flags,
-          VMM_PAGE_SIZE_2MB
+          MM_PAGE_2MB
         );
       }
     }
   }
 
-  page_table_entry *pml2 = pd_get_or_create(pml3, a, pml3_index(va), VMM_PML3, table_flags);
-  if (size != VMM_PAGE_SIZE_4KB) {
-    pml2[pml2_index(va)] = (page_table_entry)(pa.addr | flags | PT_FLAG_LARGE);
+  page_table_entry *pml2 = pd_get_or_create(pml3, pml3_index(va), PML3, table_flags);
+  if (size != MM_PAGE_4KB) {
+    pml2[pml2_index(va)] = (page_table_entry)(pa.addr | flags | MM_PT_FLAG_LARGE);
     return;
   }
 
-  page_table_entry *pml1 = pd_get_or_create(pml2, a, pml2_index(va), VMM_PML2, table_flags);
+  page_table_entry *pml1 = pd_get_or_create(pml2, pml2_index(va), PML2, table_flags);
   pml1[pml1_index(va)] = (page_table_entry)(pa.addr | flags);
 }
 
 
 void vmm_map(
-  page_map  *pmap,
-  allocator  a,
+  page_map *pmap,
 
   virtual_address  va,
   physical_address pa,
@@ -299,7 +296,7 @@ void vmm_map(
   page_size size
 ) {
   if (pmap == &kernel_page_map) spinlock_acquire(&vmm_lock);
-  vmm_map_impl(pmap, a, va, pa, flags, size);
+  vmm_map_impl(pmap, va, pa, flags, size);
   if (pmap == &kernel_page_map) spinlock_release(&vmm_lock);
 }
 
@@ -309,7 +306,7 @@ static void vmm_unmap_impl(page_map *pmap, virtual_address va) {
   page_table_entry *pml4 = NULL;
 
   switch (pmap->level) {
-    case VMM_PML5: {
+    case PML5: {
       page_table_entry *pml5 = pmap->directory;
       page_table_entry  e    = pml5[pml5_index(va)];
 
@@ -322,7 +319,7 @@ static void vmm_unmap_impl(page_map *pmap, virtual_address va) {
       break;
     }
 
-    case VMM_PML4:
+    case PML4:
       pml4 = pmap->directory;
       break;
 
@@ -396,20 +393,20 @@ static physical_address vmm_translate_impl(page_map *pmap, virtual_address va) {
   page_table_entry  e;
 
   switch (pmap->level) {
-    case VMM_PML5: {
+    case PML5: {
       page_table_entry *pml5 = pmap->directory;
       e = pml5[pml5_index(va)];
       if (!pte_present(e)) {
         panic("[vmm] vmm_translate(): invalid address at PML5 level");
       }
       if (pte_large(e)) {
-        return (physical_address) { .addr = (e & PT_PADDR_MASK) | (va.addr & (PAGE_1GB_SIZE - 1)) };
+        return (physical_address) { .addr = (e & MM_PT_PADDR_MASK) | (va.addr & (MM_PAGE_1GB - 1)) };
       }
       pml4 = pte_p2v(e);
       break;
     }
 
-    case VMM_PML4:
+    case PML4:
       pml4 = pmap->directory;
       break;
 
@@ -422,7 +419,7 @@ static physical_address vmm_translate_impl(page_map *pmap, virtual_address va) {
     panic("[vmm] vmm_translate(): invalid address at PML4 level");
   }
   if (pte_large(e)) {
-    return (physical_address) { .addr = (e & PT_PADDR_MASK) | (va.addr & (PAGE_1GB_SIZE - 1)) };
+    return (physical_address) { .addr = (e & MM_PT_PADDR_MASK) | (va.addr & (MM_PAGE_1GB - 1)) };
   }
 
   page_table_entry *pml3 = pte_p2v(e);
@@ -431,7 +428,7 @@ static physical_address vmm_translate_impl(page_map *pmap, virtual_address va) {
     panic("[vmm] vmm_translate(): invalid address at PML3 level");
   }
   if (pte_large(e)) {
-    return (physical_address) { .addr = (e & PT_PADDR_MASK) | (va.addr & (PAGE_2MB_SIZE - 1)) };
+    return (physical_address) { .addr = (e & MM_PT_PADDR_MASK) | (va.addr & (MM_PAGE_2MB - 1)) };
   }
 
   page_table_entry *pml2 = pte_p2v(e);
@@ -440,7 +437,7 @@ static physical_address vmm_translate_impl(page_map *pmap, virtual_address va) {
     panic("[vmm] vmm_translate(): invalid address at PML2 level");
   }
   if (pte_large(e)) {
-    return (physical_address) { .addr = (e & PT_PADDR_MASK) | (va.addr & (PAGE_2MB_SIZE - 1)) };
+    return (physical_address) { .addr = (e & MM_PT_PADDR_MASK) | (va.addr & (MM_PAGE_2MB - 1)) };
   }
 
   page_table_entry *pml1 = pte_p2v(e);
@@ -449,7 +446,7 @@ static physical_address vmm_translate_impl(page_map *pmap, virtual_address va) {
     panic("[vmm] vmm_translate(): invalid address at PML1 level");
   }
 
-  return (physical_address) { .addr = (e & PT_PADDR_MASK) | (va.addr & (PAGE_4KB_SIZE - 1)) };
+  return (physical_address) { .addr = (e & MM_PT_PADDR_MASK) | (va.addr & (MM_PAGE_4KB - 1)) };
 }
 
 
@@ -463,56 +460,62 @@ physical_address vmm_translate(page_map *pmap, virtual_address va) {
 
 
 // MARK: constructor
-void vmm_make_page_map(allocator a, page_map *pmap) {
+void vmm_make_page_map(page_map *pmap) {
   LIMINE_GET_RESP(paging_mode);
   assert_release(paging_mode_response != NULL);
 
   if (paging_mode_response->mode == LIMINE_PAGING_MODE_X86_64_5LVL) {
-    pmap->level = VMM_PML5;
+    pmap->level = PML5;
   }
   else {
-    pmap->level = VMM_PML4;
+    pmap->level = PML4;
   }
 
-  pmap->directory = allocate_aligned(a, PT_SIZE, PT_SIZE);
+  physical_address pa = pmm_alloc(1);
+  virtual_address  va = hhdm_p2v(pa);
+
+  pmap->directory = va.ptr;
 }
 
 
 // MARK: destroy
-static void vmm_destroy_tables(allocator a, page_table_entry *tbl, page_map_level level) {
-  if (tbl == NULL)       return;
-  if (level == VMM_PML1) return;
+static void vmm_destroy_tables(page_table_entry *tbl, page_map_level level) {
+  if (tbl == NULL)   return;
+  if (level == PML1) return;
 
   for (usize i = 0; i < 512; i++) {
     page_table_entry pte = tbl[i];
 
     if (!pte_present(pte))         continue;
     if (pte_large(pte))            continue;
-    if ((pte & PT_FLAG_USER) == 0) continue;
+    if ((pte & MM_PT_FLAG_USER) == 0) continue;
 
     page_table_entry *child = pte_p2v(pte);
 
     page_map_level next_level;
     switch (level) {
-      case VMM_PML5: next_level = VMM_PML4; break;
-      case VMM_PML4: next_level = VMM_PML3; break;
-      case VMM_PML3: next_level = VMM_PML2; break;
-      case VMM_PML2: next_level = VMM_PML1; break;
-      case VMM_PML1:
+      case PML5: next_level = PML4; break;
+      case PML4: next_level = PML3; break;
+      case PML3: next_level = PML2; break;
+      case PML2: next_level = PML1; break;
+      case PML1:
       default:
         panic("[vmm] vmm_destroy_tables(): invalid page map level");
     }
 
-    if (next_level != VMM_PML1) {
-      vmm_destroy_tables(a, child, next_level);
+    if (next_level != PML1) {
+      vmm_destroy_tables(child, next_level);
     }
 
-    deallocate(a, child, PT_SIZE);
+    virtual_address  child_vaddr = { .ptr = child };
+    physical_address child_paddr = hhdm_v2p(child_vaddr);
+
+    pmm_free(child_paddr, 1);
     tbl[i] = 0;
   }
 }
 
-void vmm_destroy_page_map(allocator a, page_map *pmap) {
+void vmm_destroy_page_map(page_map *pmap) {
   if (pmap == NULL || pmap->directory == NULL) {
     return;
   }
@@ -521,16 +524,19 @@ void vmm_destroy_page_map(allocator a, page_map *pmap) {
     panic("[vmm] vmm_destroy_page_map(): attempt to destroy kernel page map");
   }
 
-  vmm_destroy_tables(a, pmap->directory, pmap->level);
+  vmm_destroy_tables(pmap->directory, pmap->level);
 
-  deallocate(a, pmap->directory, PT_SIZE);
+  virtual_address  va = { .ptr = pmap->directory };
+  physical_address pa = hhdm_v2p(va);
+
+  pmm_free(pa, 1);
   pmap->directory = NULL;
 }
 
 
 // MARK: clone
-static void vmm_clone_tables(allocator a, page_table_entry *dst_tbl, page_table_entry *src_tbl, page_map_level level) {
-  const pt_flags user_table_flags = PT_USER_TABLE_FLAGS;
+static void vmm_clone_tables(page_table_entry *dst_tbl, page_table_entry *src_tbl, page_map_level level) {
+  const pt_flags user_table_flags = MM_PT_USER_TABLE_FLAGS;
 
   for (usize i = 0; i < 512; i++) {
     page_table_entry src_pte = src_tbl[i];
@@ -538,7 +544,7 @@ static void vmm_clone_tables(allocator a, page_table_entry *dst_tbl, page_table_
       continue;
     }
 
-    if ((src_pte & PT_FLAG_USER) == 0) {
+    if ((src_pte & MM_PT_FLAG_USER) == 0) {
       dst_tbl[i] = src_pte;
       continue;
     }
@@ -548,11 +554,9 @@ static void vmm_clone_tables(allocator a, page_table_entry *dst_tbl, page_table_
       continue;
     }
 
-    page_table_entry *child = allocate_aligned(a, PT_SIZE, PT_SIZE);
-    memset(child, 0, PT_SIZE);
-
-    virtual_address  child_vaddr = { .ptr = child };
-    physical_address child_paddr = hhdm_v2p(child_vaddr);
+    physical_address  child_paddr = pmm_alloc(1);
+    virtual_address   child_vaddr = hhdm_p2v(child_paddr);
+    page_table_entry *child       = child_vaddr.ptr;
 
     dst_tbl[i] = (page_table_entry)(child_paddr.addr | user_table_flags);
 
@@ -560,24 +564,27 @@ static void vmm_clone_tables(allocator a, page_table_entry *dst_tbl, page_table_
 
     page_map_level next_level;
     switch (level) {
-      case VMM_PML5: next_level = VMM_PML4; break;
-      case VMM_PML4: next_level = VMM_PML3; break;
-      case VMM_PML3: next_level = VMM_PML2; break;
-      case VMM_PML2: next_level = VMM_PML1; break;
-      case VMM_PML1:
+      case PML5: next_level = PML4; break;
+      case PML4: next_level = PML3; break;
+      case PML3: next_level = PML2; break;
+      case PML2: next_level = PML1; break;
+      case PML1:
       default:
         panic("[vmm] vmm_clone_tables(): invalid page map level");
     }
 
-    vmm_clone_tables(a, child, src_child, next_level);
+    vmm_clone_tables(child, src_child, next_level);
   }
 }
 
 
-void vmm_clone_page_map(allocator a, page_map *dst, page_map *src) {
-  dst->level     = src->level;
-  dst->directory = allocate_aligned(a, PT_SIZE, PT_SIZE);
-  memset(dst->directory, 0, PT_SIZE);
+void vmm_clone_page_map(page_map *dst, page_map *src) {
+  dst->level = src->level;
 
-  return vmm_clone_tables(a, dst->directory, src->directory, dst->level);
+  physical_address pa = pmm_alloc(1);
+  virtual_address  va = hhdm_p2v(pa);
+
+  dst->directory = va.ptr;
+
+  return vmm_clone_tables(dst->directory, src->directory, dst->level);
 }
