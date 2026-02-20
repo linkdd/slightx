e6c3f4010493f1452989acfb0e75bc42b392ff93
David Delassus <david.jose.delassus@gmail.com>
2026-02-20 04:39:10 +0000

    :lock: make kernel logger thread safe

--- sources/klibc/include/klibc/io/log.h
+++ sources/klibc/include/klibc/io/log.h
@@ -3,5 +3,7 @@
 #include <klibc/types.h>
 
 
+void klogger_init(void);
+
 void klog (const char *fmt, ...);
 void vklog(const char *fmt, va_list ap);
--- sources/klibc/src/io/log.c
+++ sources/klibc/src/io/log.c
@@ -2,9 +2,18 @@
 #include <klibc/mem/str.h>
 #include <klibc/algo/conv.h>
 #include <klibc/algo/format.h>
+#include <klibc/sync/lock.h>
 
 
 extern void console_write(str s);
+
+
+static spinlock log_lock = {};
+
+
+void klogger_init(void) {
+  spinlock_init(&log_lock);
+}
 
 
 void klog(const char *fmt, ...) {
@@ -23,10 +32,16 @@ static void log_formatter(void *udata, str chunk) {
 
 
 void vklog(const char *fmt, va_list ap) {
+  spinlock_acquire(&log_lock);
+
   formatter f = {
     .consume = log_formatter,
     .udata   = NULL,
   };
 
   formatter_apply_v(&f, fmt, ap);
+
+  console_write(str_literal("\n"));
+
+  spinlock_release(&log_lock);
 }
