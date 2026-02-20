8cd32ba3a751fde5741dfc77b530e910b094c91b
David Delassus <david.jose.delassus@gmail.com>
2026-02-20 14:51:45 +0000

    :sparkles: add constants related to syscalls via MSR

--- sources/kernel/include/kernel/cpu/msr.h
+++ sources/kernel/include/kernel/cpu/msr.h
@@ -8,6 +8,13 @@
 #define IA32_KERNEL_GS_BASE 0xC0000102
 #define IA32_APIC_BASE      0x1B
 
+#define IA32_EFER           0xC0000080
+#define IA32_STAR           0xC0000081
+#define IA32_LSTAR          0xC0000082
+#define IA32_FMASK          0xC0000084
+
+#define IA32_EFER_SCE       (1ULL << 0)  // System Call Extensions
+
 
 u64  rdmsr(u64 msr);
 void wrmsr(u64 msr, u64 value);
