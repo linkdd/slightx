2e8a8e92d74c05f7de1058ade5cee85c957452ec
David Delassus <david.jose.delassus@gmail.com>
2026-03-04 02:26:10 +0000

    :hammer: do not compress initial ramdisk

--- build/scripts/mkinitrd.cmake
+++ build/scripts/mkinitrd.cmake
@@ -1,8 +1,8 @@
-set(INITRD_PATH ${SYSROOT}/iso/boot/initrd.tar.gz)
+set(INITRD_PATH ${SYSROOT}/iso/boot/initrd.tar)
 
 message("-- Generating initrd")
 execute_process(
-  COMMAND ${CMAKE_COMMAND} -E tar "cfz" ${INITRD_PATH} .
+  COMMAND ${CMAKE_COMMAND} -E tar "cf" ${INITRD_PATH} .
   WORKING_DIRECTORY ${SYSROOT}/initrd
   COMMAND_ERROR_IS_FATAL ANY
 )
