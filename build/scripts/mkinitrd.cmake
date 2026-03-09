0ef7eb26276c68f211b6e02f818f519854437031
David Delassus <david.jose.delassus@gmail.com>
2026-03-09 02:21:18 +0000

    :hammer: use real `tar` to set proper file ownership when creating the initrd

--- build/scripts/mkinitrd.cmake
+++ build/scripts/mkinitrd.cmake
@@ -2,7 +2,13 @@ set(INITRD_PATH ${SYSROOT}/iso/boot/initrd.tar)
 
 message("-- Generating initrd")
 execute_process(
-  COMMAND ${CMAKE_COMMAND} -E tar "cf" ${INITRD_PATH} .
+  COMMAND tar
+          --format=ustar
+          --owner=root:0
+          --group=root:0
+          --sort=name
+          -cf ${INITRD_PATH}
+          .
   WORKING_DIRECTORY ${SYSROOT}/initrd
   COMMAND_ERROR_IS_FATAL ANY
 )
